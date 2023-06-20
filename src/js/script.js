0d6e36cc7727e90e46f296cd8bae967f544672b9
Amrita Pathak <amritavidhate@gmail.com>
2023-06-20 15:12:44 +0200

    minor bugs fixed, append child removed

--- src/js/script.js
+++ src/js/script.js
@@ -3,10 +3,11 @@ const searchInputBox = document.querySelector(".searchBox");
 const searchBtn = document.querySelector(".searchBtn");
 searchBtn.addEventListener('click', clickHandler);
 
+const movieTable = document.querySelector("#movie-table");
 const tblHeading = document.querySelector("#title");
-const tbldata1 = document.querySelector("#poster");
-const tbldata2 = document.querySelector("#year");
-const tbldata3 = document.querySelector("#actors");
+const tbldata1 = document.querySelector("#poster img");
+const tbldata2 = document.querySelector("#year h3");
+const tbldata3 = document.querySelector("#actors h4");
 
 async function clickHandler(){
   const inputBoxValue = searchInputBox.value;
@@ -21,36 +22,24 @@ async function clickHandler(){
   const movieActor = jsonResponse.description[0]["#ACTORS"];
   const movieImage = jsonResponse.description[0]["#IMG_POSTER"];
 
+  // movie table //
+  movieTable.classList.add('movie-data');
+
   // movie title //
-  const td_title = document.createElement('h1');
-  td_title.classList.add('movie-heading');
-  td_title.innerText = `Title :${movieTitle}`;
-  tblHeading.appendChild(td_title);
+  tblHeading.classList.add('movie-heading');
+  tblHeading.innerText = `Title: ${movieTitle}`;
 
   // movie poster //
-  const td_poster = document.createElement('img');
-  td_poster.classList.add('movie-img');
-  td_poster.setAttribute('src', `${movieImage}`);
-  tbldata1.appendChild(td_poster);
+  tbldata1.classList.add('movie-img');
+  tbldata1.setAttribute('src', `${movieImage}`);
 
   // movie year //
-  const td_year = document.createElement('h4');
-  td_year.classList.add('movie-year');
-  td_year.innerText = `Year :${movieYear}`;
-  tbldata2.appendChild(td_year);
+  tbldata2.classList.add('movie-year');
+  tbldata2.innerText = `Year: ${movieYear}`;
 
   // movie actors //
 
-  const td_actors = document.createElement('h3');
-  td_actors.classList.add('movie-actors');
-  td_actors.innerText = `Actors :${movieActor}`;
-  tbldata3.appendChild(td_actors);
-
-
-
-
-
-
-
+  tbldata3.classList.add('movie-actors');
+  tbldata3.innerText = `Actors: ${movieActor}`;
 
 }
