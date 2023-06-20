c0668a68d5242da4c75cf8af5f13cd304db29efc
Amrita Pathak <amritavidhate@gmail.com>
2023-06-20 14:50:50 +0200

    new elements created, classes added and appended all to table

--- src/js/script.js
+++ src/js/script.js
@@ -15,16 +15,39 @@ async function clickHandler(){
   const response = await fetch(`https://search.imdbot.workers.dev/?q=${inputBoxValue}`)
   const jsonResponse = await response.json();
 
+  // accessing objects //
   const movieTitle = jsonResponse.description[0]["#TITLE"];
   const movieYear = jsonResponse.description[0]["#YEAR"];
   const movieActor = jsonResponse.description[0]["#ACTORS"];
   const movieImage = jsonResponse.description[0]["#IMG_POSTER"];
 
-// movie title //
+  // movie title //
   const td_title = document.createElement('h1');
   td_title.classList.add('movie-heading');
   td_title.innerText = `Title :${movieTitle}`;
   tblHeading.appendChild(td_title);
+
+  // movie poster //
+  const td_poster = document.createElement('img');
+  td_poster.classList.add('movie-img');
+  td_poster.setAttribute('src', `${movieImage}`);
+  tbldata1.appendChild(td_poster);
+
+  // movie year //
+  const td_year = document.createElement('h4');
+  td_year.classList.add('movie-year');
+  td_year.innerText = `Year :${movieYear}`;
+  tbldata2.appendChild(td_year);
+
+  // movie actors //
+
+  const td_actors = document.createElement('h3');
+  td_actors.classList.add('movie-actors');
+  td_actors.innerText = `Actors :${movieActor}`;
+  tbldata3.appendChild(td_actors);
+
+
+
 
 
 
